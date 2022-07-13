7b749eb5bae9c6809beff0736a4139c92bd4bcdb
Коржов Александр <a.korzhov@ase-ec.ru>
2022-07-13 17:40:13 +0300

    delete error page

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ServersLifeTimeWebApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
