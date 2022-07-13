42bde0949741698d1bf91bb32836275ba6a5ae49
Коржов Александр <a.korzhov@ase-ec.ru>
2022-07-13 18:30:28 +0300

    add get all servers action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ServersLifeTimeWebApp.Data;
 
 namespace ServersLifeTimeWebApp.Controllers;
 
 public class HomeController : Controller
 {
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
+    {
+        _context = context;
+    }
 
     public IActionResult Index()
     {
         return View();
     }
+
+    // GET: home/getservers
+    public async Task<IActionResult> GetServers() => Ok(await _context.Servers.AsNoTracking().ToListAsync());
 }
