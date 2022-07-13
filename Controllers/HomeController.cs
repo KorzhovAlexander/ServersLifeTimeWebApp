80ba8b076e390980e97fa100b3e198f309179447
Коржов Александр <a.korzhov@ase-ec.ru>
2022-07-13 18:35:37 +0300

    add remove server action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,5 +19,20 @@ public class HomeController : Controller
     }
 
     // GET: home/getservers
+    [HttpGet]
     public async Task<IActionResult> GetServers() => Ok(await _context.Servers.AsNoTracking().ToListAsync());
+
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> RemoveServer(int id)
+    {
+        var server = await _context.Servers.FindAsync(id);
+
+        if (server is null) return NotFound();
+
+        server.IsSelectedForRemove = true;
+        await _context.SaveChangesAsync();
+
+        //todo: removing server job start
+        return Ok();
+    }
 }
